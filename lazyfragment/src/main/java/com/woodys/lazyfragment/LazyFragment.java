282a2aa93135e391a0c7c4338e435d4e58c49b88
tao.yue <12345678>
2017-06-28 16:25:25 +0800

    修改bug： 1.修改在没有设置懒加载开关的时候，导致加载不出来数据

--- lazyfragment/src/main/java/com/woodys/lazyfragment/LazyFragment.java
+++ lazyfragment/src/main/java/com/woodys/lazyfragment/LazyFragment.java
@@ -3,7 +3,6 @@ package com.woodys.lazyfragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
@@ -28,14 +27,15 @@ public abstract class LazyFragment extends BaseFragment {
     private boolean isInit = false;//真正要显示的View是否已经被初始化（正常加载）
     private Bundle savedInstanceState;
     public static final String INTENT_BOOLEAN_LAZYLOAD = "intent_boolean_lazyLoad";
+    private boolean isSetVisibleToUser = false;//判断是否设置过setUserVisibleHint()的值。
     private boolean isVisibleToUser = false;
-    private boolean isLazyLoad = true;
+    private boolean isLazyLoad = false;
     private FrameLayout layout;
     private boolean isStart = false;//是否处于可见状态，in the screen
 
     @Override
     protected final void onCreateView(Bundle savedInstanceState) {
-        if (DEBUG) Log.d(TAG, "onCreateView() : " + "isVisibleToUser:" + isVisibleToUser);
+        if (DEBUG) Log.d(TAG, "onCreateView() : " + "isVisibleToUser:" + getVisibleHint());
         super.onCreateView(savedInstanceState);
         Bundle bundle = getArguments();
         if (bundle != null) {
@@ -44,7 +44,7 @@ public abstract class LazyFragment extends BaseFragment {
         //判断是否懒加载
         if (isLazyLoad) {
             //一旦isVisibleToUser==true即可对真正需要的显示内容进行加载(注意：在FragmentStatePagerAdapter里面getUserVisibleHint()获取会出问题)
-            if (isVisibleToUser && !isInit) {
+            if (getVisibleHint() && !isInit) {
                 this.savedInstanceState = savedInstanceState;
                 onCreateViewLazy(savedInstanceState);
                 isInit = true;
@@ -72,12 +72,49 @@ public abstract class LazyFragment extends BaseFragment {
         super.onActivityCreated(savedInstanceState);
     }
 
+    /**
+     * 获取懒加载等待界面
+     * @return
+     */
     protected abstract View onCreateLazyoadingView();
+
+    //当Fragment被滑到可见的位置时，调用
+    protected void onFragmentStartLazy() {
+        if (DEBUG) Log.d(TAG, "onFragmentStartLazy() called with: " + "");
+    }
+
+    //当Fragment被滑到不可见的位置，offScreen时，调用
+    protected void onFragmentStopLazy() {
+        if (DEBUG) Log.d(TAG, "onFragmentStopLazy() called with: " + "");
+    }
+
+    protected void onCreateViewLazy(Bundle savedInstanceState) {
+        if (DEBUG) Log.d(TAG, "onCreateViewLazy() called with: " + "savedInstanceState = [" + savedInstanceState + "]");
+    }
+
+    protected void onResumeLazy() {
+        if (DEBUG) Log.d(TAG, "onResumeLazy() called with: " + "");
+    }
+
+    protected void onPauseLazy() {
+        if (DEBUG) Log.d(TAG, "onPauseLazy() called with: " + "");
+    }
+
+    protected void onDestroyViewLazy() {}
+
+    /**
+     * 判断当前fragment的显示状态
+     * @return
+     */
+    private boolean getVisibleHint(){
+        return isSetVisibleToUser?isVisibleToUser:getUserVisibleHint();
+    }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
         if (DEBUG) Log.d(TAG, "setUserVisibleHint() called with: " + "isVisibleToUser = [" + isVisibleToUser + "]");
+        this.isSetVisibleToUser = true;
         //一旦isVisibleToUser==true即可进行对真正需要的显示内容的加载
         this.isVisibleToUser = isVisibleToUser;
         //可见，但还没被初始化
@@ -128,9 +165,9 @@ public abstract class LazyFragment extends BaseFragment {
     @Deprecated
     @Override
     public final void onStart() {
-        if (DEBUG) Log.d(TAG, "onStart() : " + "getUserVisibleHint():" + getUserVisibleHint());
+        if (DEBUG) Log.d(TAG, "onStart() : " + "getUserVisibleHint():" + getVisibleHint());
         super.onStart();
-        if (isInit && !isStart && getUserVisibleHint()) {
+        if (isInit && !isStart && getVisibleHint()) {
             isStart = true;
             onFragmentStartLazy();
         }
@@ -140,43 +177,17 @@ public abstract class LazyFragment extends BaseFragment {
     @Override
     public final void onStop() {
         super.onStop();
-        if (DEBUG) Log.d(TAG, "onStop() called: " + "getUserVisibleHint():" + getUserVisibleHint());
-        if (isInit && isStart && getUserVisibleHint()) {
+        if (DEBUG) Log.d(TAG, "onStop() called: " + "getUserVisibleHint():" + getVisibleHint());
+        if (isInit && isStart && getVisibleHint()) {
             isStart = false;
             onFragmentStopLazy();
         }
     }
 
-    //当Fragment被滑到可见的位置时，调用
-    protected void onFragmentStartLazy() {
-        if (DEBUG) Log.d(TAG, "onFragmentStartLazy() called with: " + "");
-    }
-
-    //当Fragment被滑到不可见的位置，offScreen时，调用
-    protected void onFragmentStopLazy() {
-        if (DEBUG) Log.d(TAG, "onFragmentStopLazy() called with: " + "");
-    }
-
-    protected void onCreateViewLazy(Bundle savedInstanceState) {
-        if (DEBUG) Log.d(TAG, "onCreateViewLazy() called with: " + "savedInstanceState = [" + savedInstanceState + "]");
-    }
-
-    protected void onResumeLazy() {
-        if (DEBUG) Log.d(TAG, "onResumeLazy() called with: " + "");
-    }
-
-    protected void onPauseLazy() {
-        if (DEBUG) Log.d(TAG, "onPauseLazy() called with: " + "");
-    }
-
-    protected void onDestroyViewLazy() {
-
-    }
-
     @Override
     @Deprecated
     public final void onResume() {
-        if (DEBUG) Log.d(TAG, "onResume() : " + "getUserVisibleHint():" + getUserVisibleHint());
+        if (DEBUG) Log.d(TAG, "onResume() : " + "getUserVisibleHint():" + getVisibleHint());
         super.onResume();
         if (isInit) {
             onResumeLazy();
@@ -186,7 +197,7 @@ public abstract class LazyFragment extends BaseFragment {
     @Override
     @Deprecated
     public final void onPause() {
-        if (DEBUG) Log.d(TAG, "onPause() : " + "getUserVisibleHint():" + getUserVisibleHint());
+        if (DEBUG) Log.d(TAG, "onPause() : " + "getUserVisibleHint():" + getVisibleHint());
         super.onPause();
         if (isInit) {
             onPauseLazy();
@@ -196,12 +207,13 @@ public abstract class LazyFragment extends BaseFragment {
     @Override
     @Deprecated
     public final void onDestroyView() {
-        if (DEBUG) Log.d(TAG, "onDestroyView() : " + "getUserVisibleHint():" + getUserVisibleHint());
+        if (DEBUG) Log.d(TAG, "onDestroyView() : " + "getUserVisibleHint():" + getVisibleHint());
         super.onDestroyView();
         if (isInit) {
             onDestroyViewLazy();
         }
         isInit = false;
         isVisibleToUser = false;
+        isSetVisibleToUser = false;
     }
 }
